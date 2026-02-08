931dd7e524930c03d8268d43322cd305331416e1
jairejustin <justinjairebanzuela.bunos25@bicol-u.edu.ph>
2026-02-08 11:44:40 +0800

    style: run formatter

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import "./index.css";
 import App from "./App.tsx";
 
 if (import.meta.env.DEV) {
-  console.log('Running in development mode');
-  import('react-scan').then(({ scan }) => {
+  console.log("Running in development mode");
+  import("react-scan").then(({ scan }) => {
     scan({
       enabled: true,
     });
   });
 
-  import('web-vitals').then(({ onCLS, onLCP, onINP }) => {
+  import("web-vitals").then(({ onCLS, onLCP, onINP }) => {
     onCLS(console.log);
     onLCP(console.log);
     onINP(console.log);
